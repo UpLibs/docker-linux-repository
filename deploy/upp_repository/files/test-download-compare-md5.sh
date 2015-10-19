67c6e458178642c948d7bed813a2696a9a5154b5
Fernando Melo <fernando@uppoints.com>
2015-10-19 20:03:07 -0200

    Work in progress

--- deploy/upp_repository/files/test-download-compare-md5.sh
+++ deploy/upp_repository/files/test-download-compare-md5.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-FILENAME=$(aws s3 ls s3://upp-linux-repository.uppoints.com/system/ --human-readable | awk 'END{print $5}')
+ARCHFILE=$(aws s3 ls s3://upp-linux-repository.uppoints.com/system/ --human-readable | awk 'END{print $5}')
+UDOODUAL=$(aws s3 ls s3://upp-linux-repository.uppoints.com/boot/udoo/dual/ --human-readable | awk 'END{print $5}')
+UDOOQUAD=$(aws s3 ls s3://upp-linux-repository.uppoints.com/boot/udoo/quad/ --human-readable | awk 'END{print $5}')
 DATE=$(date +%Y%m%d%H%M)
 
 source ./upp-snapshot-functions.sh
-upp_downloadLastS3Arch $FILENAME ArchLinuxARM-armv7-"$DATE".tar.gz
+
+# upp_downloadAndCompare $ARCHFILE ArchLinuxARM-armv7-"$DATE".tar.gz http://archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz system archlinux
+# upp_downloadAndCompare $UDOODUAL u-boot-dual-"$DATE".imx http://archlinuxarm.org/os/imx6/boot/udoo/u-boot-dual.imx boot/udoo/dual u-boot-dual
+# upp_downloadAndCompare $UDOOQUAD u-boot-quad-"$DATE".imx http://archlinuxarm.org/os/imx6/boot/udoo/u-boot-quad.imx boot/udoo/quad u-boot-quad
+
+upp_download $ARCHFILE ArchLinuxARM-armv7-"$DATE".tar.gz http://archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz system
+upp_download $UDOODUAL u-boot-dual-"$DATE".imx http://archlinuxarm.org/os/imx6/boot/udoo/u-boot-dual.imx boot/udoo/dual
+upp_download $UDOOQUAD u-boot-quad-"$DATE".imx http://archlinuxarm.org/os/imx6/boot/udoo/u-boot-quad.imx boot/udoo/quad
+
+upp_compareMD5 $ARCHFILE ArchLinuxARM-armv7-"$DATE".tar.gz archlinux
+upp_compareMD5 $UDOODUAL u-boot-dual-"$DATE".imx u-boot-dual
+upp_compareMD5 $UDOOQUAD u-boot-quad-"$DATE".imx u-boot-quad
+
+exit 0
--- deploy/upp_repository/files/upp-snapshot-functions.sh
+++ deploy/upp_repository/files/upp-snapshot-functions.sh
@@ -1,34 +1,65 @@
-# DATE=$(date +%Y%m%d%H%M)
-
-upp_downloadLastS3Arch()
+upp_download()
 {
-
-	mkdir download
-
-	aws s3api get-object --bucket upp-linux-repository.uppoints.com --key system/"$1" "$1"
-
-	wget http://archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz -O download/"$2" > /dev/null 2>&1
-
-	md5sum "$1" > download/MD5SUM
 	
-	cd download
-	sed -i s/"$1"/"$2"/g MD5SUM
+	[ ! -d download ] && mkdir download
 
-	STATUS=$(md5sum -c MD5SUM | awk '{print $2}')
+	aws s3api get-object --bucket upp-linux-repository.uppoints.com --key "$4"/"$1" "$1"
+
+	wget "$3" -O download/"$2" > /dev/null 2>&1
+
+	# md5sum "$1" > download/"$5"-md5
+
+	# cd download
+	
+	# sed -i s/"$1"/"$2"/g "$5"-md5
+
+	# STATUS=$(md5sum -c "$5"-md5 | awk '{print $2}')
+
+	# cd ..
+
+	# # if [ "$STATUS" == "OK" ]
+	# if [ "$STATUS" == "SUCESSO" ]
+	# then
+	# 	echo "$2" "É IGUAL A" "$1"
+	# 	# exit 0
+	# fi
+
+	# # if [ "$STATUS" != "OK" ]
+	# if [ "$STATUS" != "SUCESSO" ]
+	# then
+	# 	echo "$2" "É DIFERENTE DE" "$1"
+	# 	# exit 0
+	# fi
+}
+
+export -f upp_download
+
+upp_compareMD5() 
+{
+	md5sum "$1" > download/"$5"-md5
+
+	cd download
+
+	sed -i s/"$1"/"$2"/g "$3"-md5
+
+	STATUS=$(md5sum -c "$3"-md5 | awk '{print $2}')
+
+	cd ..
 
 	# if [ "$STATUS" == "OK" ]
 	if [ "$STATUS" == "SUCESSO" ]
 	then
-		echo "ESTÁ OK!"
-		exit 0
+		echo "$2" "É IGUAL A" "$1"
+		# exit 0
 	fi
 
 	# if [ "$STATUS" != "OK" ]
 	if [ "$STATUS" != "SUCESSO" ]
 	then
-		echo "ERRO!"
-		exit 0
+		echo "$2" "É DIFERENTE DE" "$1"
+		# exit 0
 	fi
+
 }
 
-export -f upp_downloadLastS3Arch
+export -f upp_compareMD5
