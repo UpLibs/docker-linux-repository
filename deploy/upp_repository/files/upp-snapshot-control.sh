1abac7243cddc05258c3f3d5b980fd3fe49fdb7f
Fernando Melo <fernando@uppoints.com>
2015-12-18 17:54:44 -0200

    Fixes the renaming of files

--- deploy/upp_repository/files/upp-snapshot-control.sh
+++ deploy/upp_repository/files/upp-snapshot-control.sh
@@ -134,11 +134,11 @@ mkdir -p $ARM_DIR
 cd $ARM_DIR
 for file in `find . -type d | awk -F "/" '{print $2}'`
 do
-find . -iname "$file.*" -exec echo "rename -f 's/$file\.abs$/$file\.ver$DATE\.abs/' {}" \; | bash
-find . -iname "$file.*" -exec echo "rename -f 's/$file\.abs\./$file\.ver$DATE\.abs\./' {}" \; | bash
+find . -iname "$file.abs" -exec echo "rename -f 's/$file\.abs$/$file\.ver$DATE\.abs/' {}" \; | bash
+find . -iname "$file.abs.*" -exec echo "rename -f 's/$file\.abs\./$file\.ver$DATE\.abs\./' {}" \; | bash
 
-find . -iname "$file.*" -exec echo "rename -f 's/$file\.db$/$file\.ver$DATE\.db/' {}" \; | bash
-find . -iname "$file.*" -exec echo "rename -f 's/$file\.db\./$file\.ver$DATE\.db\./' {}" \; | bash
+find . -iname "$file.db" -exec echo "rename -f 's/$file\.db$/$file\.ver$DATE\.db/' {}" \; | bash
+find . -iname "$file.db.*" -exec echo "rename -f 's/$file\.db\./$file\.ver$DATE\.db\./' {}" \; | bash
 
 find . -iname "$file.*" -exec echo "rename -f 's/$file\.files$/$file\.ver$DATE\.files/' {}" \; | bash
 find . -iname "$file.*" -exec echo "rename -f 's/$file\.files\./$file\.ver$DATE\.files\./' {}" \; | bash
