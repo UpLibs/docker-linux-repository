275111e9d833583c0636b0b25de1cfbeddf2c873
Julio Cesar Fiamoncini <jc.fiamoncini@gmail.com>
2017-04-27 15:27:00 -0300

    Refactory and add Dowlnload Sellinabox and Bats

---
+++ deploy/upp_repository/files/upp-arch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DATE=$(date +%Y%m%d%H%M)
+
+## DOWNLOAD ARCH
+cd $S3_DIR/
+mkdir -p $SYSTEM_DIR/
+mkdir -p $SYSTEM_DIR/armv7h
+
+## armv7
+wget http://archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz -O $SYSTEM_DIR/armv7h/ArchLinuxARM-armv7-"$DATE".tar.gz > /dev/null 2>&1
+
+## rpi-2
+mkdir -p $SYSTEM_DIR/armv7h/rpi-2
+wget http://archlinuxarm.org/os/ArchLinuxARM-rpi-2-latest.tar.gz -O $SYSTEM_DIR/armv7h/rpi-2/ArchLinuxARM-armv7-rpi-2-"$DATE".tar.gz > /dev/null 2>&1
+
+## VERIFY ARCH FROM S3
+ARCHARMV7=$(aws s3 ls s3://$S3_BUCKET/system/armv7h/ --human-readable | awk 'END{print $5}')
+if [ -n "$ARCHARMV7" ]
+then
+	upp_download $ARCHARMV7 $SYSTEM_DIR/armv7h $S3_BUCKET system/armv7h
+	upp_compareMD5 $ARCHARMV7 $SYSTEM_DIR/armv7h ArchLinuxARM-armv7-"$DATE".tar.gz
+fi
+
+ARCHRPI2=$(aws s3 ls s3://$S3_BUCKET/system/armv7h/rpi-2/ --human-readable | awk 'END{print $5}')
+if [ -n "$ARCHRPI2" ]
+then
+	upp_download $ARCHRPI2 $SYSTEM_DIR/armv7h/rpi-2 $S3_BUCKET system/armv7h/rpi-2
+	upp_compareMD5 $ARCHRPI2 $SYSTEM_DIR/armv7h/rpi-2 ArchLinuxARM-armv7-rpi-2-"$DATE".tar.gz
+fi
