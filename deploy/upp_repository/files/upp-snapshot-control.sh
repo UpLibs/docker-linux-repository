7adf22e1c8ce86e3fa6b9d58048dbb66a34f2cfa
Fernando Melo <fernando@uppoints.com>
2017-05-17 19:30:47 -0300

    log para verificar diretório atual

--- deploy/upp_repository/files/upp-snapshot-control.sh
+++ deploy/upp_repository/files/upp-snapshot-control.sh
@@ -121,8 +121,13 @@ mkdir -p $PACKAGES_DIR/snapshots
 mkdir -p $PACKAGES_DIR/snapshots/armv7h
 mkdir -p $PACKAGES_DIR/snapshots/armv7h/{downloaded,not_downloaded}
 
+pwd >> $REPOSITORY_DIR/dir_"$DATE".log
+
 cd $PACKAGES_DIR/
 wget -nH -N -r --no-parent $URL_MIRROR > snapshot_"$DATE".txt 2>&1
+
+pwd >> $REPOSITORY_DIR/dir_"$DATE".log
+ll >> $REPOSITORY_DIR/dir_"$DATE".log
 
 ## FILTER
 cat snapshot_"$DATE".txt | grep saved | awk '{print $6}' > ./snapshots/armv7h/downloaded/downloaded_packages_"$DATE".txt
@@ -130,11 +135,15 @@ cat snapshot_"$DATE".txt | grep 'not retrieving.' | awk '{print $8}' > ./snapsho
 sed -i s/[\“\”\‘\’]/\'/g ./snapshots/armv7h/downloaded/downloaded_packages_"$DATE".txt
 sed -i s/[\“\”\‘\’]/\'/g ./snapshots/armv7h/not_downloaded/aint_downloaded_packages_"$DATE".txt
 
+pwd >> $REPOSITORY_DIR/dir_"$DATE".log
+
 ## ORGANIZING
 rm snapshot_*.txt
 cat ./snapshots/armv7h/downloaded/downloaded_packages_"$DATE".txt ./snapshots/armv7h/not_downloaded/aint_downloaded_packages_"$DATE".txt | sort > ./snapshots/armv7h/snapshot_"$DATE".txt
 
 # upp_verifySnapshotLogSize
+
+pwd >> $REPOSITORY_DIR/dir_"$DATE".log
 
 ## RENAME STATIC FILES
 mkdir -p $ARM_DIR
@@ -155,6 +164,8 @@ done
 
 ## SYNC
 cd $REPOSITORY_DIR/
+
+pwd >> $REPOSITORY_DIR/dir_"$DATE".log
 
 /sbin/aws s3 sync $S3_DIR/ s3://$S3_BUCKET --acl public-read > sync_"$DATE".log
 
