5d4e502e3a0dddca0b824be312c97c5fc9dccf47
Fernando Melo <fernando@uppoints.com>
2017-05-22 15:52:18 -0300

    configuração da aws cli movida para env.txt do docker

--- deploy/upp_repository/files/upp-snapshot-control.sh
+++ deploy/upp_repository/files/upp-snapshot-control.sh
@@ -12,9 +12,6 @@ ARM_DIR="$PACKAGES_DIR/armv7h"
 # ARQUIVO GERADO MANUALMENTE: env > /var/cache/repository/env.txt
 source $REPOSITORY_DIR/env.txt
 
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-
 upp_download()
 {
 	mkdir "$2"/temp
